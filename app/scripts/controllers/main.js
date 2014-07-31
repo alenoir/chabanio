944c36de25a48938f8b273717eeb69d6d0ffa004
alenoir <antoine@yupeek.com>
2014-07-31 11:14:22 +0200

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name instamozappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the instamozappApp
+ */
+angular.module('instamozappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
