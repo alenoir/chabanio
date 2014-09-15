05a435a75ccf4545b57aad1129ca18271cc1df5b
alenoir <antoine.alenoir@gmail.com>
2014-09-15 20:10:46 +0200

    change api url production

---
+++ app/config.js
@@ -0,0 +1,40 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    angular: "../bower_components/angular/angular",
+    "angular-animate": "../bower_components/angular-animate/angular-animate",
+    "angular-cookies": "../bower_components/angular-cookies/angular-cookies",
+    "angular-facebook": "../bower_components/angular-facebook/lib/angular-facebook",
+    "angular-mocks": "../bower_components/angular-mocks/angular-mocks",
+    "angular-moment": "../bower_components/angular-moment/angular-moment",
+    "angular-resource": "../bower_components/angular-resource/angular-resource",
+    "angular-route": "../bower_components/angular-route/angular-route",
+    "angular-sanitize": "../bower_components/angular-sanitize/angular-sanitize",
+    "angular-scenario": "../bower_components/angular-scenario/angular-scenario",
+    "angular-socialshare": "../bower_components/angular-socialshare/angular-socialshare",
+    "angular-touch": "../bower_components/angular-touch/angular-touch",
+    affix: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix",
+    alert: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert",
+    button: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button",
+    carousel: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel",
+    collapse: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse",
+    dropdown: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown",
+    tab: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab",
+    transition: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition",
+    scrollspy: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy",
+    modal: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal",
+    tooltip: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip",
+    popover: "../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover",
+    "es5-shim": "../bower_components/es5-shim/es5-shim",
+    fullpage: "../bower_components/fullpage.js/jquery.fullPage",
+    json3: "../bower_components/json3/lib/json3",
+    moment: "../bower_components/moment/moment",
+    "onepage-scroll": "../bower_components/onepage-scroll/jquery.onepage-scroll",
+    restangular: "../bower_components/restangular/dist/restangular"
+  },
+  packages: [
+
+  ]
+});
---
+++ app/scripts/controllers/feed.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name instamozappApp.controller:FeedCtrl
+ * @description
+ * # FeedCtrl
+ * Controller of the instamozappApp
+ */
+angular.module('instamozappApp')
+  .controller('FeedCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
